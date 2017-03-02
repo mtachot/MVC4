03f821bcf46c4d4cb607e5db54a3c2e3b25c71ac
mtachot <matthieu.tachot@hotmail.fr>
2017-03-02 14:42:55 +0100

    Solving Webconfig problem

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MaDeuxiemeAction()
+        {
+            return View();
+        }
+
+        public ActionResult Contact(int id = 0)
+        {
+            return View(id);
+        }
+    }
+}
---
+++ MvcApplication1/Controllers/MonDeuxiemeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class MonDeuxiemeController : Controller
+    {
+        //
+        // GET: /MonDeuxieme/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string test2()
+        { 
+            return "mon test avec un string"; 
+        }
+
+    }
+}
